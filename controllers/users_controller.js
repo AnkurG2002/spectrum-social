cef07c76eb3a5ce18533003396d20b68538fe05f
Ankur <ankurgoswami718@gmail.com>
2022-12-23 23:39:21 +0530

    authentication completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,6 +9,10 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Spectrum | Sign Up"
     });
@@ -16,6 +20,10 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Spectrum | Sign In"
     });
@@ -49,5 +57,14 @@ module.exports.create = function(req, res){
 
 //  sign in and create a session for the user   
 module.exports.createSession = function(req, res){
-    
+    return res.redirect('/');
 };
+
+module.exports.destroySession = function(req, res, next){
+    req.logout(function(err){
+        if(err){
+            return next(err);
+        }
+        return res.redirect('/');
+    });
+}
--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
-
 
 app.use(express.static('./assets'));
 
@@ -18,13 +32,42 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
 
 // set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+// mongo store is usedto store the sessoin cookie in the db
+app.use(session({
+    name: 'spectrum',
+    // todo change the secret before deployment in production mode
+    secret: 'i have a file on you',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://127.0.0.1/spectrum_development',
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
